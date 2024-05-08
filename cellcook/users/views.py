d8f9243a5560b4ab731661f2b9aecc288b86f0ea
Seou0912 <myuni0912@gmail.com>
2024-05-08 16:36:07 +0900

    0508 views.py add

--- cellcook/users/views.py
+++ cellcook/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .models import User
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        id = request.POST['id']
+        password = request.POST['password']
+        email = request.POST.get('email')  # email is optional
+        user = User.objects.create_user(id=id, password=password, email=email)
+        return redirect('login')
+    else:
+        return render(request, 'signup')
+
+def login_view(request):
+    if request.method == 'POST':
+        id = request.POST['id']
+        password = request.POST['password']
+        user = authenticate(request, id=id, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            return render(request, 'login', {'error': 'Invalid login'})
+    else:
+        return render(request, 'login.html')
